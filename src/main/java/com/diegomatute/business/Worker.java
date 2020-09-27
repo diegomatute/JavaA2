e13e63b617a079c0ca7d4d0d902bb4fe822e8c74
diegomatute <Dmatute43@gmail.com>
2020-09-27 17:52:29 -0400

    Packages, source files, and private variables were added.

---
+++ src/main/java/com/diegomatute/business/Worker.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.diegomatute.business;
+
+/**
+ *
+ * @author diegomatute
+ */
+public class Worker 
+{
+    private String First;
+    private String Last;
+    private int Id;
+    private double PayRate; 
+    
+}
